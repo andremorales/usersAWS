1832adebcbcf92f05edea23dc1bf908e90346fd1
Unknown <andremorales10@hotmail.com>
2018-06-15 01:43:29 -0300

    inclusao API e Form

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,6 @@
  </template>
 <script>
 export default {
-  name: 'App',
+  name: "App"
 };
 </script>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,14 +1,37 @@
 <template>
 <v-app>
-  <h1>users</h1>
-  </v-app>
+  <v-card>
+    <v-form v-model="valid">
+      <v-text-field
+      v-model="body.email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+      ></v-text-field>
+        <v-btn 
+        block 
+        color="secondary" 
+        dark
+        >
+          <v-icon 
+            dark
+          >add</v-icon
+        >ADD</v-btn>
+    </v-form>
+  </v-card>
+</v-app>
 </template>
-
 <script>
 export default {
-    name : "Users",
+  name: "Users",
+  body: {
+    email: null
+  }
 };
+axios.post(
+  "https://cajxts600j.execute-api.us-east-1.amazonaws.com/morales",
+  body
+);
 </script>
-
 <style scoped>
 </style>
